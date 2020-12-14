a7ad89a9ca0727e03aea700631ea131b66e85a6e
Seytric <chasemccoy.a@gmail.com>
2020-12-14 11:40:51 -0700

    it works but its been butchered

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'package:bulletin/LoginRegisterPage.dart';
-import 'package:bulletin/Password.dart';
 import 'package:bulletin/AddPhoto.dart';
 import 'package:bulletin/Home.dart';
 import 'package:bulletin/Loading.dart';
 import 'package:bulletin/Pinned.dart';
-import 'package:bulletin/ReportBug.dart';
-import 'package:bulletin/Settings.dart';
-import 'package:bulletin/Tos.dart';
-import 'package:bulletin/ContactUs.dart';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth_oauth/firebase_auth_oauth.dart';
-import 'package:firebase_database/firebase_database.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-
-
-//IM FUCKING GAY
-
+//import 'package:bulletin/Settings.dart';
 
 void main() => runApp(MaterialApp(
     routes: {
@@ -30,11 +13,10 @@ void main() => runApp(MaterialApp(
       '/home': (context) => Home(),
       '/pinned': (context) => Pinned(),
       '/addphoto': (context) => AddPhoto(),
-      '/settings': (context) => Settings(),
-      '/tos': (context) => Tos(),
-      '/password': (context) => Password(),
-      '/reportbug': (context) => ReportBug(),
-      '/contact': (context) => Contact(),
+    //  '/settings': (context) => Settings(),
+     // '/tos': (context) => Tos(),
+     // '/password': (context) => Password(),
+      //'/reportbug': (context) => ReportBug(),
       //'/communities': (context) => Communities(),   add later
 
 
