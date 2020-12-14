4007c151b4107256c6d14410c3ee094de1343002
Seytric <chasemccoy.a@gmail.com>
2020-12-13 19:36:04 -0700

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+
 import 'package:bulletin/LoginRegisterPage.dart';
 import 'package:bulletin/Password.dart';
-import 'package:flutter/material.dart';
 import 'package:bulletin/AddPhoto.dart';
 import 'package:bulletin/Home.dart';
 import 'package:bulletin/Loading.dart';
@@ -8,9 +9,18 @@ import 'package:bulletin/Pinned.dart';
 import 'package:bulletin/ReportBug.dart';
 import 'package:bulletin/Settings.dart';
 import 'package:bulletin/Tos.dart';
-import 'package:bulletin/Contacty.dart';
+import 'package:bulletin/ContactUs.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_oauth/firebase_auth_oauth.dart';
+import 'package:firebase_database/firebase_database.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 
+//IM FUCKING GAY
 
 
 void main() => runApp(MaterialApp(
